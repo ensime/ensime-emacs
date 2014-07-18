9b2232ae0e99aa3ef17ae9ed081302ebf7bf24fa
hmgibson23 <hugo.gibson@yahoo.co.uk>
2014-07-18 15:06:41 +0100

    Added error message if the sbt command is not found

--- ensime-mode.el
+++ ensime-mode.el
@@ -517,7 +517,7 @@ CACHE-DIR is the server's persistent output directory."
 	(append-to-file (concat "\njavaOptions += \"" flag "\"\n") nil buildfile))
       (message "Starting an ENSIME server in %s" buffer)
       (if (executable-find ensime-sbt-command)
-        (comint-exec (current-buffer) buffer ensime-sbt-command nil (list "run"))
+          (comint-exec (current-buffer) buffer ensime-sbt-command nil (list "run"))
         (error "sbt command not found")))
     (let ((proc (get-buffer-process (current-buffer))))
       (ensime-set-query-on-exit-flag proc)
