8471c165837816522e6a88ef0ad17d050ef32d6f
Daniel Mijares <daniel.j.mijares@gmail.com>
2015-10-07 12:24:26 -0400

    **FINALLY** added a prompt
    better handling of obtaining ensime config file

--- ensime-sbt.el
+++ ensime-sbt.el
@@ -97,30 +97,120 @@
   (interactive)
   (sbt-command "package"))
 
-(defun ensime-sbt-find-subproject (file-name source-set config)
-  (when (and config source-set)
-    (let* ((subprojects (plist-get config :subprojects))
-	   (matches-subproject-dir? (lambda (dir)
-				      (string-match-p dir file-name)))
+;; shameless copypasta from magit-utils.el
+;; added one little thing to eval the dynamic submodule binding
+(defmacro ensime-sbt-read-char-prompt (prompt verbose &rest clauses)
+  "Prompts and reads for a character defined in CLAUSES.
+
+CLAUSES can either be an associative list of list clauses
+(char prompt string), or it can be something that when evaluated
+returns an associative list of the same structure.  Prompting for
+modules is done dynamically, and uses the latter, while other
+prompting requires no evaluation and is done up front."
+  (let* ((first-clause (car clauses))
+	 (clauses (if (and (sequencep first-clause)
+			   (= 3 (length first-clause)))
+		      clauses
+		    (eval clauses))))
+    (declare (indent 2)
+	     (debug (form form &rest (characterp form body))))
+    `(pcase (read-char-choice
+	     (concat ,prompt
+		     ,(concat (mapconcat 'cadr clauses ", ")
+			      (and verbose ", or [C-g] to abort") " "))
+	     ',(mapcar 'car clauses))
+       ,@(--map `(,(car it) ,@(cddr it)) clauses))))
+
+(defun ensime-sbt-module-prompt (module abbrev li ri)
+  "Makes the module prompt string.
+
+SUBPROJECT is the module name, i.e. `core' ABBREV is the
+abbreviation character for the prompt, i.e. `?o' LI AND RI are
+the indices to the left and right of the ABBREV character in the
+string.
+
+Results in the same module name, but with brackets enclosing the
+abbreviation character."
+  (let ((left (s-left li module))
+	(prompt-abbrev (char-to-string abbrev))
+	(right (-> (- (length module) ri) (s-right module))))
+    (s-concat left "[" prompt-abbrev "]" right)))
+
+(defun ensime-sbt-prompt-char-indices (module li ri module-prompt-clauses)
+  "Finds the char abbreviation and indices of this MODULE.
+
+MODULE is the module name, i.e. `core'.
+MODULE-PROMPT-CLAUSES is a
+potentially empty associative list that is of the form
+  (char module-prompt module).
+
+Looks at the character defined at left and right indices (`LI'
+and `RI'), makes sure that character isn't already used as a key
+in the MODULE-PROMPT-CLAUSES If no key is defined for that
+character use it, otherwise move on to the next character and try
+again."
+  (let ((abbrev (-> (substring module li ri) string-to-char)))
+    (if (eq nil (assoc abbrev module-prompt-clauses))
+	(list abbrev li ri)
+      (ensime-sbt-prompt-char-indices module (+ 1 li) (+ 1 ri) module-prompt-clauses))))
+
+(defun ensime-sbt-module-prompt-clauses ()
+  "Creates the module prompt associative list, used during prompting.
+
+From ensime config file, get subproject names, and transform them
+into an associative list with elements
+  (char-abbrev module-prompt-string module-name)"
+  (let* ((config (-> (ensime-connection) ensime-config))
+	 (subprojects (->> (plist-get config :subprojects)
+			   (-map (lambda (sp) (plist-get sp :name)))))
+	 (module-clauses
+	  (let (prompt-clauses)
+	    (-each subprojects
+	      (lambda (sp)
+		(let* ((ai (ensime-sbt-prompt-char-indices sp 0 1 prompt-clauses))
+		       (prompt (apply 'ensime-sbt-module-prompt sp ai))
+		       (new-clauses (cons (list (car ai) prompt sp) prompt-clauses)))
+		  (setq prompt-clauses new-clauses))))
+	    prompt-clauses)))
+    module-clauses))
+
+(defun ensime-sbt-prompt-for-test ()
+  "Prompt sequence when `*-test-dwim' can't figure out what to do."
+  (let ((module
+	 (ensime-sbt-read-char-prompt
+	  "Do you want to run from module " t ensime-sbt-module-prompt-clauses))
+	(source-set
+	 (ensime-sbt-read-char-prompt "Do you want to run from " t
+				      (?t "[t]est" "")
+				      (?i "[i]t" "it:")
+				      (?f "[f]un" "fun:")))
+	(task
+	 (ensime-sbt-read-char-prompt "Do you want to run " t
+				      (?t "[t]est" "test")
+				      (?o "test-[o]nly" "testOnly")
+				      (?q "test-[q]uick" "testQuick"))))
+    (concat module "/" source-set task)))
+
+(defun ensime-sbt-find-subproject (file-name source-set)
+  (when source-set
+    (let* ((config (-> (ensime-connection) ensime-config))
+	   (subprojects (plist-get config :subprojects))
+	   (matches-subproject-dir? (lambda (dir) (string-match-p dir file-name)))
 	   (find-subproject (lambda (sp)
 			      (-any matches-subproject-dir? (plist-get sp :source-roots)))))
-      (-> (-find find-subproject subprojects)
-	  (plist-get :name)))))
+      (-> (-find find-subproject subprojects) (plist-get :name)))))
 
 (defun ensime-sbt-test-dwim (command)
   (let* ((file-name (or buffer-file-name default-directory))
 	 (source-set (cond
 		      ((string-match-p "src/test" file-name) "")
 		      ((string-match-p "src/it" file-name) "it:")
-		      ((string-match-p "src/fun" file-name) "fun:")))
-	 (config (if (ensime-connected-p)
-		     (ensime-config)
-		   (let ((f (ensime-config-find)))
-		     (when f (ensime-config-load f)))))
-	 (subproject (ensime-sbt-find-subproject file-name source-set config)))
-    (if (and source-set subproject)
-	(sbt-command (concat subproject "/" source-set command))
-      (hydra-ensime-sbt-test/body))))
+		      ((string-match-p "src/fun" file-name) "fun:"))))
+    (if source-set
+	(-> (ensime-sbt-find-subproject file-name source-set)
+	    (concat "/" source-set command)
+	    sbt-command)
+      (-> (ensime-sbt-prompt-for-test) sbt-command))))
 
 (defun ensime-sbt-do-test-dwim ()
   (interactive)
@@ -139,32 +229,6 @@
 	 (command (concat "test-only" " " cleaned-class)))
     (ensime-sbt-test-dwim command)))
 
-(defun ensime-guess-project-dir ()
-  (let ((ensime-file (ensime-config-find-file
-		      (or buffer-file-name default-directory))))
-    (if ensime-file
-	(file-name-directory ensime-file)
-      "Not Found")))
-
-(defhydra hydra-ensime-sbt-test (:hint nil :exit t)
-  "
-    ENSIME Project: %(ensime-guess-project-dir)
-
-^Unit^              ^Integration^           ^Functional^
-^^^^^^^^----------------------------------------------------
-_t_: test           _it_: it:test           _ft_: fun:test
-_k_: testQuick      _ik_: it:testQuick      _fk_: fun:testQuick
-
-----------------------------------------------------
-
-"
-  ("t"  (sbt-command "test"))
-  ("k"  (sbt-command "testQuick"))
-  ("it" (sbt-command "it:test"))
-  ("ik" (sbt-command "it:testQuick"))
-  ("ft" (sbt-command "fun:test"))
-  ("fk" (sbt-command "fun:testQuick"))
-  ("q" nil "cancel"))
 
 (provide 'ensime-sbt)
 
